2c83efae0ed911f2e8fcea9a6fd40854d7cc989f
anby-dmr <13177850569@163.com>
2025-05-08 16:45:10 +0800

    做了几件事，大概主要就是验证了直觉和算法有效性？把LQR的时域T拉大之后，求出的Q、F拿去非线性那边跑，在±4°之内是可以控住的。但是这个不是目前最重要的。

--- control_lib/rdp_control_nlsys_parallel.py
+++ control_lib/rdp_control_nlsys_parallel.py
@@ -78,7 +78,7 @@ def solve_ocp(x0, cartpole_sys, timepts, Q, R, Qf, lower, upper):
     constraints = [opt.input_range_constraint(cartpole_sys, lower, upper)]
     running_cost = opt.quadratic_cost(cartpole_sys, Q, R)
     terminal_cost = opt.quadratic_cost(cartpole_sys, Qf, None)
-    result = opt.solve_ocp(cartpole_sys, timepts, x0, cost=running_cost, terminal_cost=terminal_cost)
+    result = opt.solve_ocp(cartpole_sys, timepts, x0, cost=running_cost, trajectory_constraints=constraints, terminal_cost=terminal_cost)
     return result
 
 """
@@ -161,7 +161,7 @@ def solve_multi_mpc(initial_states, cartpole_sys, Q, R, Qf, MPC_T, T, u_lower, u
 if __name__ == '__main__':
     # Experiment params
     epochs = 50
-    batch_size = 32
+    batch_size = 1
     lr = 0.01
     max_workers = 7
     test_name = 'OCP300_degree15_linearInit'
@@ -174,16 +174,26 @@ if __name__ == '__main__':
     cartpole_sys = get_cartpole_sys()
     if load_params:
         print("Loading params....")
-        Q_data = torch.load('D:/Docs/code_lib/graduation_test/Q_tensor_test_5dim.pth')
-        F_data = torch.load('D:/Docs/code_lib/graduation_test/Qf_tensor_5dim.pth')
+        # Q_data = torch.load('D:/Docs/code_lib/graduation_test/Q_tensor_test_5dim.pth').to(device)
+        # F_data = torch.load('D:/Docs/code_lib/graduation_test/Qf_tensor_5dim.pth').to(device)
+        Q_data = torch.tensor([[-1.7734e-02, -8.8569e-02,  2.4331e-04,  7.7437e-01,  9.5532e-01],
+        [-1.2750e-02,  2.5982e-02,  1.9449e-04, -4.0806e-01,  2.6715e-01],
+        [-1.4288e-02, -7.5010e-02,  3.0087e-04,  3.0775e-01,  7.7770e-01],
+        [-2.0422e-02,  4.9010e-02, -1.5912e-04, -4.1044e-01,  3.3453e-01],
+        [-2.8305e-02,  7.2792e-03,  1.4851e-04, -4.3044e-01,  8.1992e-01]]).to(device)
+        F_data = torch.tensor([[-0.0550, -0.0332,  0.2356,  1.3808,  1.6040],
+        [-0.0108, -0.1860, -0.1911,  3.3550,  1.8036],
+        [ 0.1025, -0.6454,  0.0852,  2.1742,  1.2791],
+        [-0.1784,  0.6742,  0.0060,  2.4674,  1.4243],
+        [-0.0565,  0.0411, -0.0784,  1.6239,  1.4481]]).to(device)
     else:
         Q_data = torch.randn(5, 5, device=device)
         F_data = torch.randn(5, 5, device=device)
     Q = nn.Parameter(Q_data)
     R = torch.Tensor([[1.]]).to(device)
     F = nn.Parameter(F_data)
-    MPC_T = 100
-    T = 30
+    MPC_T = 300
+    T = 300
     u_lower = -100
     u_upper = 100
 
